4f5ca6e73baf626a7f273863dcdf644667652d41
Liad Khamdadash <liadkh95@gmail.com>
2019-06-08 21:34:11 +0300

    add shell theme

---
+++ build-me-ubuntu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo bash -c 'echo "set completion-ignore-case On" >> /etc/inputrc'
+
+cd /home/`whoami`/
+mkdir .themes
+cd .themes/
+git clone https://github.com/vinceliuice/Mojave-gtk-theme.git
+cd Mojave-gtk-theme/
+./install.sh 
+cd 
+
+cd /home/`whoami`/
+mkdir .icons
+cd .icons/
+git clone https://github.com/zayronxio/Mojave-CT.git
+cd
+
+cd /home/`whoami`/
+cd .icons/
+mkdir capitaine-cursors-git
+mkdir capitaine-cursors
+cd capitaine-cursors-git/
+git clone https://github.com/keeferrourke/capitaine-cursors.git
+cp -pr dist/ ~/.icons/capitaine-cursors
+cp -pr dist-white/ ~/.icons/capitaine-cursors-White
+cd
+
+cp -pr ./my-shell-theme /home/`whoami`/.themes
